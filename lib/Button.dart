a6a995d7cc24279a97b0b71c4138959c57adbbbc
meera1012 <meeradogra321@gmail.com>
2024-08-20 10:40:39 +0530

    Update Button.dart

--- lib/Button.dart
+++ lib/Button.dart
@@ -9,6 +9,7 @@ class Button extends StatelessWidget{
       margin: EdgeInsets.symmetric(horizontal: 50),
       decoration: BoxDecoration(
         color: Colors.cyanAccent,
+        backgroundColor: Colors.black
         borderRadius: BorderRadius.circular(10),
       ),
       child: Center(
